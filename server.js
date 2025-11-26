ceaeedfe9a493ae91fe2a3dcb8c662cfc8888c1f
hana <hanna@circle-vn.com>
2025-11-26 14:58:57 +0700

    init code

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = express();
+
+const PORT = 3055;
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        console.log('Server is closed');
+    });
+});
+
